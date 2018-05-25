9a4e996970ddeb218a4d9003723ea926e0739e69
Dan Trickey <dan@trickey.io>
2018-05-25 17:56:23 +0100

    Added parts and semesters

--- reslib/course.py
+++ reslib/course.py
@@ -6,9 +6,83 @@ from .io import loadfile
 class Course:
 
     def __init__(self, folder):
-
         self.data = loadfile(os.path.join(folder, "course.yml"))
 
     @property
     def name(self):
         return self.data["name"]
+
+    @property
+    def parts(self):
+        parts = []
+        for part in self.data["parts"]:
+            parts.append(Part(part))
+        return parts
+
+    @property
+    def score(self):
+
+        if len(self.parts) <= 0:
+            return 0
+
+        score = 0
+
+        for part in self.parts:
+            score += part.score * part.weight
+        return score
+
+    @property
+    def classification(self):
+        return "Fail"
+
+
+class Part:
+
+    def __init__(self, data):
+        self.data = data
+
+    @property
+    def name(self):
+        return self.data["name"]
+
+    @property
+    def weight(self):
+        return self.data["weight"]
+
+    @property
+    def score(self):
+
+        if len(self.semesters) <= 0:
+            return 0
+
+        score = 0
+
+        for sem in self.semesters:
+            score += sem.score
+        score /= len(self.semesters)
+        return score
+
+    @property
+    def semesters(self):
+        semesters = []
+        for sem in self.data["semesters"]:
+            semesters.append(Semester(sem))
+        return semesters
+
+
+class Semester:
+
+    def __init__(self, data):
+        self.data = data
+
+    @property
+    def name(self):
+        return self.data["name"]
+
+    @property
+    def score(self):
+        return 100
+
+    @property
+    def modules(self):
+        return self.data["modules"]
--- results.py
+++ results.py
@@ -5,10 +5,19 @@ from reslib import Course
 
 
 def print_course_info(course):
-    print("Course Name: ", course.name)
+    print("Name:", course.name)
+    print("Score:", course.score, "%")
+    print("Classification:", course.classification)
+    print("The course has", len(course.parts), "parts:")
+    for part in course.parts:
+        print("\t", part.name, "(", part.score, ")")
+        for sem in part.semesters:
+            print("\t\t", sem.name, "(", sem.score, ")")
 
 
-FUNCTION_MAP = {'info': print_course_info}
+FUNCTION_MAP = {
+    'info': print_course_info,
+}
 
 parser = argparse.ArgumentParser(description="Result Calculator")
 parser.add_argument("course_folder", help="Directory for course folder")
